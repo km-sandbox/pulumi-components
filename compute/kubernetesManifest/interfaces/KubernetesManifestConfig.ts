4ecda2bcd2bb5adcd3db6a4b354d4a1a58d5718f
kmarilleau <kevin.marilleau@gmail.com>
2023-10-17 14:40:23 +0000

    fix(compute/kubernetesManifest): :bug: rm useless provider from config
    rm useless provider from config

--- compute/kubernetesManifest/interfaces/KubernetesManifestConfig.ts
+++ compute/kubernetesManifest/interfaces/KubernetesManifestConfig.ts
@@ -1,12 +1,9 @@
-import * as k8s from '@pulumi/kubernetes';
-
 export interface KubernetesManifestConfig {
   readonly project: string;
   readonly name: string;
   readonly environment: string;
   readonly location: string;
 
-  readonly provider: k8s.Provider;
   readonly clusterName: string;
   readonly kubeConfig: string;
 }
