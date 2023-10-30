d8130161648e7f6c768400e37c021ddfeac13c4f
kmarilleau <kevin.marilleau@gmail.com>
2023-10-30 12:37:11 +0000

    clean code for resourceMock

--- __tests__/ResourceMock.ts
+++ __tests__/ResourceMock.ts
@@ -17,24 +17,47 @@ type MockConfiguration = {
   callMocks?: CallMockConfig[];
 };
 
+function resourceMockIsEqualToArgs(
+  resourceMock: ResourceMockConfig,
+  args: pulumi.runtime.MockResourceArgs
+): boolean {
+  const resourceMockType = resourceMock.provider + ':' + resourceMock.type;
+
+  return resourceMockType === args.type;
+}
+
+function callMockIsEqualToArgs(
+  callMock: CallMockConfig,
+  args: pulumi.runtime.MockCallArgs
+): boolean {
+  const callMockToken = callMock.provider + ':' + callMock.token;
+
+  return callMockToken === args.token;
+}
+
 export function setResourceMocks(config: MockConfiguration = {}): void {
   pulumi.runtime.setMocks({
     newResource: (
       args: pulumi.runtime.MockResourceArgs
     ): pulumi.runtime.MockResourceResult => {
-      const mockConfig = config.resourceMocks?.find(
-        rm => rm.provider + ':' + rm.type === args.type
+      const defaultMockResourceResult = {
+        id: `${args.name}-id`,
+        state: args.inputs,
+      };
+
+      const mockConfig = config.resourceMocks?.find(rm =>
+        resourceMockIsEqualToArgs(rm, args)
       );
-      return (
-        mockConfig?.mockResponse || {id: `${args.name}-id`, state: args.inputs}
-      );
+
+      return mockConfig?.mockResponse || defaultMockResourceResult;
     },
     call: (
       args: pulumi.runtime.MockCallArgs
     ): pulumi.runtime.MockCallResult => {
-      const mockConfig = config.callMocks?.find(
-        cm => cm.provider + ':' + cm.token === args.token
+      const mockConfig = config.callMocks?.find(cm =>
+        callMockIsEqualToArgs(cm, args)
       );
+
       return mockConfig?.mockResponse || args.inputs;
     },
   });
